ecd040c799112ea9f4679f52f25c9a3793384c68
smart-thinker46 <alimalickkweyu46@gmail.com>
2026-03-05 19:59:16 +0300

    fix(mobile): harden app entry and remove risky pre-router polyfill

--- index.tsx
+++ index.tsx
@@ -1,19 +1,16 @@
 import 'react-native-url-polyfill/auto';
-import './src/__create/polyfills';
 
 import 'expo-router/entry';
 import { type ReactNode } from 'react';
 import { AppRegistry } from 'react-native';
 import { DeviceErrorBoundaryWrapper } from './__create/DeviceErrorBoundary';
 
-if (__DEV__) {
-  function WrapperComponentProvider({
-    children,
-  }: {
-    children: ReactNode;
-  }) {
-    return <DeviceErrorBoundaryWrapper>{children}</DeviceErrorBoundaryWrapper>;
-  }
-
-  AppRegistry.setWrapperComponentProvider(() => WrapperComponentProvider);
+function WrapperComponentProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <DeviceErrorBoundaryWrapper>{children}</DeviceErrorBoundaryWrapper>;
 }
+
+AppRegistry.setWrapperComponentProvider(() => WrapperComponentProvider);
